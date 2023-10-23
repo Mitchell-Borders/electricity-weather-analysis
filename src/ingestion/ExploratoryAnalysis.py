cdb90a6fe65c06ee8c3646f68275390068121311
Mitchell-Borders <mitchell.borders28@gmail.com>
2023-10-22 21:13:22 -0500

    analysis

--- src/ingestion/ExploratoryAnalysis.py
+++ src/ingestion/ExploratoryAnalysis.py
@@ -2,3 +2,38 @@
 
 # Note - alternative approaches can be used besides local Python code.  Large datasets may require Databricks.  
 # You can also use ChatGPT or similar language model for this step. 
+
+import pandas as pd
+
+print('Weather data analysis:')
+# Read the CSV file into a DataFrame
+df = pd.read_csv('src/ingestion/ingestedData/WeatherDataPure.csv')
+
+# Display the first 5 rows of the DataFrame
+print(df.head())
+
+# Display the shape of the DataFrame (number of rows and columns)
+print(df.shape)
+
+# Display the column names of the DataFrame
+print(df.columns)
+
+# Display summary statistics for the numerical columns in the DataFrame
+print(df.describe())
+
+print('Electricity data analysis:')
+
+# Read the CSV file into a DataFrame
+df = pd.read_csv('src/ingestion/ingestedData/ElectricityDataPure.csv')
+
+# Display the first 5 rows of the DataFrame
+print(df.head())
+
+# Display the shape of the DataFrame (number of rows and columns)
+print(df.shape)
+
+# Display the column names of the DataFrame
+print(df.columns)
+
+# Display summary statistics for the numerical columns in the DataFrame
+print(df.describe())
